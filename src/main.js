bd543df68c459402211657a1a7945be579e99a88
DESKTOP-B9T2VF4\Administrator <www.knqiufan@foxmail.com>
2019-03-30 10:11:56 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 
+Vue.use(ElementUI);
+Vue.component('v-icon', Icon)
 Vue.config.productionTip = false
 
 new Vue({
